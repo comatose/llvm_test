220f3de34e2816a96b1d8d80fbf46d42d1d1eb25
comatose <comatose@comatose-VirtualBox.(none)>
2011-10-19 17:58:28 +0900

    g++ compiled files added

---
+++ add.s
@@ -0,0 +1,24 @@
+	.file	"add.cpp"
+	.text
+	.globl	_Z3addii
+	.type	_Z3addii, @function
+_Z3addii:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	12(%ebp), %eax
+	movl	8(%ebp), %edx
+	addl	%edx, %eax
+	popl	%ebp
+	.cfi_def_cfa 4, 4
+	.cfi_restore 5
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z3addii, .-_Z3addii
+	.ident	"GCC: (Ubuntu/Linaro 4.6.1-9ubuntu3) 4.6.1"
+	.section	.note.GNU-stack,"",@progbits
